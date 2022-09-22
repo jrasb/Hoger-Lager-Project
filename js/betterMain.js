6174f10e5408e39828dbf2ab1353b4eacbc94f7d
jrasb <okrypczyk@protonmail.com>
2022-09-22 21:26:46 +0200

    Minor fixes

--- js/betterMain.js
+++ js/betterMain.js
@@ -1,6 +1,6 @@
 console.log("file loaded")
 
-//querySelector const vars here
+// querySelector CONST VARS HERE
 const addFundsButton = document.querySelector(".add-funds");
 const decreaseFundsButton = document.querySelector(".decrease-funds");
 const doubleFundsButton = document.querySelector(".double-funds");
@@ -9,26 +9,44 @@ const currentFunds = document.querySelector(".currentFunds");
 const house = document.querySelector(".house");
 const higher = document.querySelector(".higher");
 const lower = document.querySelector(".lower");
+const winLossScreen = document.querySelector(".win-loss-screen")
 
-//let vars here
+// LET VARS HERE
 let playerTotal, computerTotal
     playerFunds = 0, houseFunds = 0, 
     playerDice = [], computerDice = [];
 
-//functions here
-const generateRandomNumber = function() {				//generates random number (used for the dice)
+// FUNCTIONS HERE
+// Put these into functions in case these could be used multiple times
+const losing = function() {
+	console.log("loser")
+	houseFunds -= playerFunds;
+    winLossScreen.innerHTML = "YOU LOST"
+}
+
+const winning = function() {
+	console.log("WINNER");
+	houseFunds += playerFunds;
+    winLossScreen.innerHTML = "YOU WON"
+}
+
+// Generates random number (used for the dice)
+const generateRandomNumber = function() {				
     return Math.floor(Math.random() * 6) + 1;
 }
 
-const addFunds = function() {							//adds funds to the bet in increments of 100
+// Adds funds to the bet in increments of 100
+const addFunds = function() {							
     playerFunds += 100;
     currentFunds.innerHTML = playerFunds;
     console.log(playerFunds);
 }
 
-const decreaseFunds = function() {						//removes funds from the bet in increments of 100
+// Removes funds from the bet in increments of 100
+// If-statement makes sure that you can't have a negative betting value
+const decreaseFunds = function() {
     if (playerFunds != 0) { 
-        playerFunds -= 100;								//if-statement makes sure that you can't have a negative betting value
+        playerFunds -= 100;		
     } else {
         playerFunds = playerFunds
     }
@@ -36,13 +54,15 @@ const decreaseFunds = function() {						//removes funds from the bet in incremen
     console.log(playerFunds)
 }
 
-const doubleFunds = function() {						//simple script that doubles current bet
+// Simple script that doubles current bet
+const doubleFunds = function() {
     playerFunds *= 2;
     currentFunds.innerHTML = playerFunds;
     console.log(playerFunds);
 }
 
-const updateFunds = function() {						//updates current bet (needed to return betting value to 0 after the end of the round)
+// Updates current bet (needed to return betting value to 0 after the end of the round)
+const updateFunds = function() {
     playerFunds = 0;
     currentFunds.innerHTML = playerFunds;
     house.innerHTML = houseFunds;
@@ -50,7 +70,8 @@ const updateFunds = function() {						//updates current bet (needed to return be
     console.log("win/loss: " + houseFunds);
 }
 
-const hideDice = function() {							//changes the computer dice sprites to be back to the "unknown state" when the player decides to roll the dice again
+// Changes the computer dice sprites to be back to the "unknown state" when the player decides to roll the dice again
+const hideDice = function() {
     document.querySelectorAll(".computer-dice > div").forEach(
         (element) => {
             element.className = "dice-state-unknown";
@@ -58,19 +79,19 @@ const hideDice = function() {							//changes the computer dice sprites to be ba
     );
 }
 
-const rollDice = function() {							//function used to "roll" the dice by use of the previous generateRandomNumber func
+// Function used to "roll" the dice by use of the previous generateRandomNumber func
+const rollDice = function() {
     playerTotal = 0;
     computerTotal = 0;
-
-    while (playerTotal === computerTotal) {				//while loop functions as a failsafe to make sure that the player and computer cannot have the same total dice value
+	// While loop functions as a failsafe to make sure that the player and computer cannot have the same total dice value
+    while (playerTotal === computerTotal) {				
         playerDice = [generateRandomNumber(), generateRandomNumber()]
         computerDice = [generateRandomNumber(), generateRandomNumber()];
 
         playerTotal = playerDice[0] + playerDice[1];
         computerTotal = computerDice[0] + computerDice[1];
-        //console.log(playerTotal, computerTotal);
     }
-														//Changes the sprite of the dice based on the number rolled by changing the class of the object
+	// Changes the sprite of the dice based on the number rolled by changing the class of the object
     document.querySelectorAll(".player-dice > div").forEach(
         (element, index) => {
             element.className = "dice-state-" + playerDice[index]; 
@@ -81,7 +102,8 @@ const rollDice = function() {							//function used to "roll" the dice by use of
     toggleButton();
 }
 
-const revealDice = function() {							//Changes the sprite of the dice based on the number rolled by changing the class of the object
+// Changes the sprite of the dice based on the number rolled by changing the class of the object
+const revealDice = function() {
     document.querySelectorAll(".computer-dice > div").forEach(
         (element, index) => {
             element.className = "dice-state-" + computerDice[index]; 
@@ -89,7 +111,8 @@ const revealDice = function() {							//Changes the sprite of the dice based on 
     );
 }
 
-const toggleButton = function() {						//Toggles all buttons to either enabled or disabled causing the player to be unable to change the betting value or the "re-roll" the dice
+// Toggles all buttons to either enabled or disabled causing the player to be unable to change the betting value or the "re-roll" the dice
+const toggleButton = function() {
     document.querySelectorAll("button").forEach(
         function(button) {
             button.disabled = !button.disabled;
@@ -97,16 +120,13 @@ const toggleButton = function() {						//Toggles all buttons to either enabled o
     );
 }
 
-
-const guessHigher = function() {						//functions determining whether the player has guessed correctly
+//functions determining whether the player has guessed correctly
+const guessHigher = function() {
     if (playerTotal > computerTotal) {
-        console.log("WINNER");
-        houseFunds += playerFunds;
+		winning();
         house.innerHTML = houseFunds;
-        
     } else {
-        console.log("loser")
-        houseFunds -= playerFunds;
+		losing();
         house.innerHTML = houseFunds;
     }
     
@@ -117,12 +137,10 @@ const guessHigher = function() {						//functions determining whether the player
 
 const guessLower = function() {
     if (playerTotal < computerTotal) {
-        console.log("WINNER");
-        houseFunds += playerFunds;
+		winning();
         house.innerHTML = houseFunds;
     } else {
-        console.log("loser")
-        houseFunds -= playerFunds;
+		losing();
         house.innerHTML = houseFunds;
     }
 
